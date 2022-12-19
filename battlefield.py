35615de082cb8b484b648046e16f37828c35555e
unknown <greateducation123@yahoo.com>
2022-12-19 16:36:40 -0600

    I updated more info in Battlefield

--- battlefield.py
+++ battlefield.py
@@ -3,16 +3,36 @@ from robot import Robot
 
 
 def __init__(self):
-    name = ('Killer')    
+    pass
 
 
+def run_game(self):
+    dinosaur = Dinosaur('Killer', '', '30')
+    dinosaur.self.name = ('Killer')
+    dinosaur.self.health = 
+    dinosaur.self.set_attack = ('30')
+
+def display_welcome(self):
+    print('\nWelcome to Dinosaurs vs Robots!\nOnly one can win!')
+
+def battle_phase(self):
+    pass
+
+def display_winner(self):
+    pass
 
 
+# class Robot:
+#     def __init__(self, name, health, active_weapon):
+#         self.name = name
+#         self.health = health
+#         self.active_weapon = Weapon("sword", 30)
 
+        
+#     def attack(self, dinosaur): 
+#         pass
 
-
-
-    # class Dinosaur:
+  # class Dinosaur:
     # def __init__(self, name, health, attack_power):
     #     self.name = name
     #     self.health = health
@@ -23,11 +43,11 @@ def __init__(self):
     #     pass
 
 
-    from alarm_clock import Alarm_Clock
+#     from alarm_clock import Alarm_Clock
 
-clock = Alarm_Clock('10:00', True, '4:00')
+# clock = Alarm_Clock('10:00', True, '4:00')
 
 
-clock.set_or_change_time('10:00')
-clock.set_alarm_time('4:00')
-clock.set_alarm_on_or_off()
+# clock.set_or_change_time('10:00')
+# clock.set_alarm_time('4:00')
+# clock.set_alarm_on_or_off()
